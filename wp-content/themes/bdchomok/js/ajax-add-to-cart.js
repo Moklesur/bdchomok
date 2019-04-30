e838dc4a63532c0014b63d2f092974c29e639a86
azhar <azharulIslmanipu@gmail.com>
2019-05-01 00:12:04 +0600

    Add to cart js added

---
+++ wp-content/themes/bdchomok/js/ajax-add-to-cart.js
@@ -0,0 +1,48 @@
+(function ($) {
+
+    $(document).on('click', '.ajax_add_to_cart', function (e) {
+        e.preventDefault();
+
+        var $thisbutton = $(this),
+            product_sku = $thisbutton.attr('data-product_sku'),
+            product_qty =$thisbutton.attr('data-quantity'),
+            product_id = $thisbutton.attr('data-product_id'),
+            variation_id =$thisbutton.attr('data-variation_id');
+
+        var data = {
+            action: 'woocommerce_ajax_add_to_cart',
+            product_id: product_id,
+            product_sku: product_sku,
+            quantity: product_qty,
+            variation_id: variation_id,
+        };
+
+
+        $(document.body).trigger('adding_to_cart', [$thisbutton, data]);
+
+        $.ajax({
+            type: 'post',
+            url: wc_add_to_cart_params.ajax_url,
+            data: data,
+            beforeSend: function (response) {
+                $thisbutton.removeClass('added').addClass('loading');
+            },
+            complete: function (response) {
+
+                $thisbutton.addClass('added').removeClass('loading');
+            },
+            success: function (response) {
+
+                if (response.error & response.product_url) {
+                    window.location = response.product_url;
+                    return;
+                } else {
+                    $(document.body).trigger('added_to_cart', [response.fragments, response.cart_hash, $thisbutton]);
+                }
+            },
+        });
+
+        return false;
+    });
+
+})(jQuery);
