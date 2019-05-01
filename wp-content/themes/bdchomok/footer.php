3ca9ec59821fb7cd4720c24ef4a98b8791b0de46
azhar <azharulIslmanipu@gmail.com>
2019-05-02 05:36:59 +0600

    Quick View Completed

--- wp-content/themes/bdchomok/footer.php
+++ wp-content/themes/bdchomok/footer.php
@@ -26,6 +26,22 @@
 
 </footer><!-- #colophon -->
 </div><!-- #page -->
+
+<div class="modal fade bd-example-modal-lg quick-view-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header ml-auto">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="modal fade bd-example-modal-lg product-card-model" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg modal-dialog-centered">
         <div class="modal-content p-5">
