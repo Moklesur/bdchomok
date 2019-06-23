a4a0d5d7531c5708ed358933fbb1c13551133ca2
azhar <azharulIslmanipu@gmail.com>
2019-06-23 18:20:52 +0600

    some change

--- wp-content/themes/bdchomok/ajax-search/functions.php
+++ wp-content/themes/bdchomok/ajax-search/functions.php
@@ -22,7 +22,7 @@ function advance_search(){
             while ($productLoop->have_posts()) : $productLoop->the_post();
 
                 // Product Data
-                if ($product_count <= 2 ) {
+                if ($product_count <= 10 ) {
                     $product_count++;
                     if (has_post_thumbnail($productLoop->post->ID)) {
                         $productImage = get_the_post_thumbnail($productLoop->post->ID, array(60, 60), array('class' => 'img-fluid'));
@@ -30,6 +30,7 @@ function advance_search(){
 
                     $terms = get_the_terms($productLoop->post->ID, 'product_cat');
 
+                    $product = wc_get_product( $productLoop->post->ID );
                     $category_name = [];
                     $termLink = [];
                     foreach ($terms as $term) {
@@ -46,6 +47,7 @@ function advance_search(){
                     </div>
                     <a href="'.get_post_permalink($productLoop->post->id).'" class="product-title mr-lg-5 mr-sm-2 mr-1 load_popup" data-pid="'.$productLoop->post->ID.'">' . get_the_title() . '</a>
                     <a href="'.site_url().'/product-category/'.$termLink.'" class="product-cat mr-lg-5 mr-sm-2 mr-1">' . implode(', ', $category_name) . '</a>
+                    <p> tk. '.$product->get_regular_price().'</p>
                     <p class="mb-0 highlight"><span></span></p></div>';
                 }
 
@@ -53,7 +55,7 @@ function advance_search(){
             endwhile;
             wp_reset_postdata();
 
-            if ($productLoop->post_count > 2) {
+            if ($productLoop->post_count > 10) {
                 $output .= ' <div class="show-more-search"><span><span></span></span><a href="'.get_bloginfo('url').'?s='.$search_term.'&p_type=product">Show all ' . $productLoop->post_count . ' results</a></div>';
             }
         }
