a6ce21d1e69535bbf3908bb7db82b05e99ef79aa
azhar <azharulislamnipu@gmail.com>
2019-08-24 01:45:28 +0600

    UPDATED

--- wp-content/themes/bdchomok/ajax-search/functions.php
+++ wp-content/themes/bdchomok/ajax-search/functions.php
@@ -2,6 +2,8 @@
 //Add Ajax Actions
 function advance_search(){
         $search_term = esc_attr( $_POST['keyword'] );
+
+
         if(!empty($search_term)):
         $productArgs = array(
             'posts_per_page' => -1,
@@ -49,6 +51,9 @@ function advance_search(){
                         break;
                     }
 
+
+
+
                     $output .= '<div class="search-product-items  align-items-center">
                  
                               <tr class="aligncenter">
@@ -57,13 +62,13 @@ function advance_search(){
                   
                                   </td>
                                 <td width="70%">
-                                  <a href="'.get_post_permalink($productLoop->post->id).'" class="product-title d-block " data-pid="'.$productLoop->post->ID.'">' . get_the_title() .'('.strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', ' ', $post_slug))).')'.'</a>
+                                  <a href="'.get_post_permalink($productLoop->post->id).'" class="product-title d-block " data-pid="'.$productLoop->post->ID.'">' . get_the_title() .'('.strtolower(trim( $post_slug)).')'.'</a>
                      
                                 <a href="'.site_url().'/product-category/'.$termLink.'" class="product-cat d-block ">' . implode(', ', $category_name) . '</a>
                               
                                    </td>';
                     if ($product->get_regular_price()){
-                        $output .='  <td width="30%"> <p class="float-right"> ৳. '.$product->get_regular_price().'</p>
+                        $output .='  <td width="30%"> <p class="float-right ">  <span class="woocommerce-Price-amount amount"> '.$product->get_price().'</span> <span class="woocommerce-Price-currencySymbol">৳&nbsp;</span></p>
                    </td>';
                     }
 
